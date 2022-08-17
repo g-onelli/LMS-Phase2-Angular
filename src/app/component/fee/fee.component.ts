8441921ceba47991177808af13c766052da9d990
Ian Kelly <ikelly@uwyo.edu>
2022-08-17 13:53:00 -0400

    fees reflect and update patron balance

--- src/app/component/fee/fee.component.ts
+++ src/app/component/fee/fee.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { PatronEditDto } from 'src/app/auth/model/user.model';
 import { FeeModel } from 'src/app/model/fee.model';
+import { Patron } from 'src/app/model/patron.model';
 import { FeeService } from 'src/app/service/fee.service';
+import { PatronService } from 'src/app/service/patron.service';
 
 @Component({
   selector: 'app-fee',
@@ -13,18 +16,34 @@ export class FeeComponent implements OnInit {
   emptyFee: boolean;
   balance: number;
 
-  constructor(private feeService: FeeService) { }
+  patrons: Patron[];
+  patronToEdit: Patron;
+  patronToEditDto: PatronEditDto;
+
+  constructor(private feeService: FeeService, private patronService: PatronService) { }
 
   ngOnInit(): void {
     this.emptyFee = false;
+    
+    this.patronService.getAllPatrons(0, 1000).subscribe({
+      next: (data) => {
+        this.patrons = data;
+      },
+      error: (e) => {
+        //redirect to error page
+      }
+    });
+
     this.feeService.getFees().subscribe({
       next: (data) => {
-        if(data[0].id != null) {
+        if (data[0].id != null) {
           this.fees = data;
           this.balance = data[0].patronBalance;
+          console.log(data[0].patronBalance);
         } else {
           this.emptyFee = true;
           this.balance = data[0].patronBalance;
+          console.log(data[0].patronBalance);
         }
         this.feeService.fee$.next(this.fees);
       },
@@ -39,6 +58,28 @@ export class FeeComponent implements OnInit {
   }
 
   markPaid(id: number) {
+    this.patronToEditDto = new PatronEditDto;
+
+    this.patrons.forEach(p => {
+      if(p.name === this.fees[id].patronName) {
+        this.patronToEdit = p;
+      }
+    })
+
+    this.balance -= eval(this.fees[id].total.toString());
+
+    this.patronToEditDto.id = this.patronToEdit.id;
+    this.patronToEditDto.name = this.patronToEdit.name;
+    this.patronToEditDto.cardexpirationdate = this.patronToEdit.cardexpirationdate;
+    this.patronToEditDto.balance = this.balance;
+    this.patronToEditDto.username = this.patronToEdit.username;
+
+    this.patronService.putPatron(this.patronToEditDto).subscribe({
+      next: (data) => {
+
+      }
+    })
+
     this.feeService.updateFee(this.fees[id]).subscribe({
       next: (data) => {
         let pName = this.fees[id].patronName;
